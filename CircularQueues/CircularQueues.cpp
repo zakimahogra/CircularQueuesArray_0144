fa540fb64c2b15017f1bda3dd198a34dc33304fe
M S I <M S I@MSI>
2024-05-31 11:31:55 +0700

    switsh case insert,remove,display

--- CircularQueues/CircularQueues.cpp
+++ CircularQueues/CircularQueues.cpp
@@ -97,4 +97,31 @@ int main() {
 			cin >> ch;
 			cout << endl;
 
-			
+			switch (ch) {
+			case'1': {
+				q.insert();
+				break;
+			}
+			case'2': {
+				q.remove();
+				break;
+			}
+			case'3': {
+				q.display();
+				break;
+			}
+			case'4': {
+				return 0;
+			}
+			default: {
+				cout << "Invalid option!!" << endl;
+				break;
+			}
+			}
+		}
+		catch (exception& e) {
+			cout << "Check for the values entered." << endl;
+		}
+	}
+	return 0;
+}
