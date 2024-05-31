0f76216df317a0cd670ebf76483a242fd8d7a958
M S I <M S I@MSI>
2024-05-31 11:29:40 +0700

    if front position

--- CircularQueues/CircularQueues.cpp
+++ CircularQueues/CircularQueues.cpp
@@ -58,4 +58,11 @@ public:
 
 		cout << "\nElements in the queue are...\n";
 
+		if (FRONT_position <= REAR_position) {
+			while (FRONT_position <= REAR_position) {
+				cout << queue_array[FRONT_position] << " ";
+				FRONT_position++;
+			}
+			cout << endl;
+		}
 		
