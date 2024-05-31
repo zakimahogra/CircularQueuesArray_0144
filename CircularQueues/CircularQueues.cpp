b62632638dc111f5087c666c2efcb978b88d4c8e
M S I <M S I@MSI>
2024-05-31 11:30:43 +0700

    else while front position dan rear position

--- CircularQueues/CircularQueues.cpp
+++ CircularQueues/CircularQueues.cpp
@@ -65,4 +65,20 @@ public:
 			}
 			cout << endl;
 		}
-		
+		else {
+			while (FRONT_position <= max - 1) {
+				cout << queue_array[FRONT_position] << " ";
+				FRONT_position++;
+			}
+
+			FRONT_position = 0;
+
+			while (FRONT_position <= REAR_position) {
+				cout << queue_array[FRONT_position] << "";
+				FRONT_position++;
+			}
+			cout << endl;
+		}
+	}
+};
+
